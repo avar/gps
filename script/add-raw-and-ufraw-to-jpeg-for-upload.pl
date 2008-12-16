09d741a52c73b6ebf690ed0d6db0826061a8e083
Ævar Arnfjörð Bjarmason <avar@aoeu.(none)>
2008-12-16 02:57:31 +0000

    a script to add the .cr2 and .ufraw files related to a .jpg image to it as a rar archive before upload

---
+++ script/add-raw-and-ufraw-to-jpeg-for-upload.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use feature ':5.10';
+
+my $directory = $ARGV[0] // '.';
+
+opendir my $dir, $directory or die "Can't open dir `$directory': $!";
+my @file = sort grep { not /^\.{1,2}$/ } readdir $dir;
+closedir $dir;
+
+my @jpeg = grep { -f and /\.jpg$/i} @file;
+
+# Go through all the JPEG files and find the equivalent CR2 and UFRAW
+# files, rar them up and append them to the JPEG
+for my $jpeg (@jpeg) {
+    my ($name, $ext) = split /\./, $jpeg;
+
+    my @append = grep { /^$name\.(?i:cr2|ufraw)$/ } @file;
+
+    $, = ",";
+    say "$jpeg -> append @append";
+
+    my $cmd;
+
+    $cmd = "rar a $name.rar @append";
+    system $cmd and die "Command `$cmd' failed with code `$?'";
+
+    $cmd = "cat $jpeg $name.rar >> $jpeg.new";
+    system $cmd and die "Command `$cmd' failed with code `$?'";
+
+    $cmd = "rm $name.rar";
+    system $cmd and die "Command `$cmd' failed with code `$?'";
+
+    $cmd = "mv $jpeg.new $jpeg";
+    warn $cmd; # and die "Command `$cmd' failed with code `$?'";
+}
+
+
